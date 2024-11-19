1a971c9986c9faaeeb21e1a61486fb5d8e214b3d
ImZirix <xxkillerxx.xg@gmail.com>
2024-11-19 13:19:16 +0300

    added fireball/lightning/earthquake/giantarrow all levels

--- js/index.js
+++ js/index.js
@@ -39,6 +39,60 @@ const hpMap = {
     1: 450,
 };
 
+const spellsAndEquipments = [
+    {
+        name: "Lightning",
+        levels: [
+            {level: 1, damage: 150},
+            {level: 2, damage: 180},
+            {level: 3, damage: 210},
+            {level: 4, damage: 240},
+            {level: 5, damage: 270},
+            {level: 6, damage: 320},
+            {level: 7, damage: 400},
+            {level: 8, damage: 480},
+            {level: 9, damage: 560},
+            {level: 10, damage: 600},
+            {level: 11, damage: 640},
+        ]
+    },
+
+    {
+        name: "Earthquake",
+        levels: [
+            {level: 1, damage: 0.145},
+            {level: 2, damage: 0.17},
+            {level: 3, damage: 0.21},
+            {level: 4, damage: 0.25},
+            {level: 5, damage: 0.29},
+        ]
+    },
+
+    {
+        name: "GiantArrow",
+        levels: [
+            {level: 1, damage: 750},
+            {level: 2, damage: 750},
+            {level: 3, damage: 850},
+            {level: 4, damage: 850},
+            {level: 5, damage: 850},
+            {level: 6, damage: 1000},
+            {level: 7, damage: 1000},
+            {level: 8, damage: 1000},
+            {level: 9, damage: 1200},
+            {level: 10, damage: 1200},
+            {level: 11, damage: 1200},
+            {level: 12, damage: 1500},
+            {level: 13, damage: 1500},
+            {level: 14, damage: 1500},
+            {level: 15, damage: 1750},
+            {level: 16, damage: 1750},
+            {level: 17, damage: 1750},
+            {level: 18, damage: 1950},
+        ]
+    }
+];
+
 slider.oninput =  function() {
     sliderLevel.innerHTML = this.value;
 
