005fc3a6a55e09fbb68d3d8eaed6f94b259a8988
ImZirix <xxkillerxx.xg@gmail.com>
2024-11-20 14:56:15 +0300

    created a new js file to store the spells and equipments information

---
+++ js/spellsAndEquipments.js
@@ -0,0 +1,87 @@
+export const spellsAndEquipments = [
+    {
+        name: "Lightning",
+        levels: [
+            {level: 1, damage: 150},
+            {level: 2, damage: 180},
+            {level: 3, damage: 210},
+            {level: 4, damage: 240},
+            {level: 5, damage: 270},
+            {level: 6, damage: 320},
+            {level: 7, damage: 400},
+            {level: 8, damage: 480},
+            {level: 9, damage: 560},
+            {level: 10, damage: 600},
+            {level: 11, damage: 640}
+        ]
+    },
+
+    {
+        name: "Earthquake",
+        levels: [
+            {level: 1, damage: 0.145},
+            {level: 2, damage: 0.17},
+            {level: 3, damage: 0.21},
+            {level: 4, damage: 0.25},
+            {level: 5, damage: 0.29}
+        ]
+    },
+
+    {
+        name: "GiantArrow",
+        levels: [
+            {level: 1, damage: 750},
+            {level: 2, damage: 750},
+            {level: 3, damage: 850},
+            {level: 4, damage: 850},
+            {level: 5, damage: 850},
+            {level: 6, damage: 1000},
+            {level: 7, damage: 1000},
+            {level: 8, damage: 1000},
+            {level: 9, damage: 1200},
+            {level: 10, damage: 1200},
+            {level: 11, damage: 1200},
+            {level: 12, damage: 1500},
+            {level: 13, damage: 1500},
+            {level: 14, damage: 1500},
+            {level: 15, damage: 1750},
+            {level: 16, damage: 1750},
+            {level: 17, damage: 1750},
+            {level: 18, damage: 1950}
+        ]
+    },
+
+    {
+        name: "Fireball",
+        levels:  [
+            {level: 1, damage: 1500},
+            {level: 2, damage: 1500},
+            {level: 3, damage: 1700},
+            {level: 4, damage: 1700},
+            {level: 5, damage: 1800},
+            {level: 6, damage: 1950},
+            {level: 7, damage: 1950},
+            {level: 8, damage: 2050},
+            {level: 9, damage: 2200},
+            {level: 10, damage: 2200},
+            {level: 11, damage: 2350},
+            {level: 12, damage: 2650},
+            {level: 13, damage: 2650},
+            {level: 14, damage: 2750},
+            {level: 15, damage: 3100},
+            {level: 16, damage: 3100},
+            {level: 17, damage: 3250},
+            {level: 18, damage: 3400},
+            {level: 19, damage: 3400},
+            {level: 20, damage: 3500},
+            {level: 21, damage: 3650},
+            {level: 22, damage: 3650},
+            {level: 23, damage: 3750},
+            {level: 24, damage: 3900},
+            {level: 25, damage: 3900},
+            {level: 26, damage: 3950},
+            {level: 27, damage: 4100}
+        ],
+    }
+
+];
