195aaf17c5b7644a28ec31d502fdeb9c453795fb
ImZirix <xxkillerxx.xg@gmail.com>
2024-11-20 14:57:57 +0300

    created a new js file to store all the building information including hp, name, level, img src

---
+++ js/buildings.js
@@ -0,0 +1,40 @@
+export const buildings = {
+    townhall: {
+        stats: [
+            { level: 1, health: 450, image: "/image/townhalls/townhall_1_logo.webp" },
+            { level: 2, health: 1600, image: "/image/townhalls/townhall_2_logo.webp" },
+            { level: 3, health: 1850, image: "/image/townhalls/townhall_3_logo.webp" },
+            { level: 4, health: 2100, image: "/image/townhalls/townhall_4_logo.webp" },
+            { level: 5, health: 2400, image: "/image/townhalls/townhall_5_logo.webp" },
+            { level: 6, health: 2800, image: "/image/townhalls/townhall_6_logo.webp" },
+            { level: 7, health: 3300, image: "/image/townhalls/townhall_7_logo.webp" },
+            { level: 8, health: 3900, image: "/image/townhalls/townhall_8_logo.webp" },
+            { level: 9, health: 4600, image: "/image/townhalls/townhall_9_logo.webp" },
+            { level: 10, health: 5500, image: "/image/townhalls/townhall_10_logo.webp" },
+            { level: 11, health: 6800, image: "/image/townhalls/townhall_11_logo.webp" },
+            { level: 12, health: 7500, image: "/image/townhalls/townhall_12_logo.webp" },
+            { level: 13, health: 8200, image: "/image/townhalls/townhall_13_logo.webp" },
+            { level: 14, health: 8900, image: "/image/townhalls/townhall_14_logo.webp" },
+            { level: 15, health: 9600, image: "/image/townhalls/townhall_15_logo.webp" },
+            { level: 16, health: 10000, image: "/image/townhalls/townhall_16_logo.webp" }
+        ]
+    },
+    monolith: {
+        stats: [
+            { level: 1, health: 4747, image: "/image/monolith/monolith_1_logo.webp" },
+            { level: 2, health: 5050, image: "/image/monolith/monolith_2_logo.webp" },
+            { level: 3, health: 5353, image: "/image/monolith/monolith_3_logo.webp" }
+        ]
+    },
+    eagleArtillery: {
+        stats: [
+            { level: 1, health: 4000, image: "/image/artillery/artillery_1_logo.webp" },
+            { level: 2, health: 4400, image: "/image/artillery/artillery_2_logo.webp" },
+            { level: 3, health: 4800, image: "/image/artillery/artillery_3_logo.webp" },
+            { level: 4, health: 5200, image: "/image/artillery/artillery_4_logo.webp" },
+            { level: 5, health: 5600, image: "/image/artillery/artillery_5_logo.webp" },
+            { level: 6, health: 5900, image: "/image/artillery/artillery_6_logo.webp" },
+            { level: 7, health: 6200, image: "/image/artillery/artillery_6_logo.webp" }
+        ]
+    }
+};
--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import { buildings } from "./buildings.js";
+import { spellsAndEquipments } from './spellsAndEquipments.js';
 const slider = document.querySelector(".slider");
 const sliderLevel = document.querySelector(".level");
 const cardLogo = document.querySelector(".cardLogo");
@@ -6,139 +8,11 @@ const spellSelect = document.querySelector(".spellSelect");
 const levelSlider =  document.querySelector(".levelSlider");
 const levelValue = document.querySelector(".levelValue");
 const damageResult = document.querySelector(".damageResult");
-const imageMap = {
-    16: "/image/townhalls/townhall_16_logo.webp",
-    15: "/image/townhalls/townhall_15_logo.webp",
-    14: "/image/townhalls/townhall_14_logo.webp",
-    13: "/image/townhalls/townhall_13_logo.webp",
-    12: "/image/townhalls/townhall_12_logo.webp",
-    11: "/image/townhalls/townhall_11_logo.webp",
-    10: "/image/townhalls/townhall_10_logo.webp",
-    9: "/image/townhalls/townhall_9_logo.webp",
-    8: "/image/townhalls/townhall_8_logo.webp",
-    7: "/image/townhalls/townhall_7_logo.webp",
-    6: "/image/townhalls/townhall_6_logo.webp",
-    5: "/image/townhalls/townhall_5_logo.webp",
-    4: "/image/townhalls/townhall_4_logo.webp",
-    3: "/image/townhalls/townhall_3_logo.webp",
-    2: "/image/townhalls/townhall_2_logo.webp",
-    1: "/image/townhalls/townhall_1_logo.webp",
-};
-const hpMap = {
-    16: 10000,
-    15: 9600,
-    14: 8900,
-    13: 8200,
-    12: 7500,
-    11: 6800,
-    10: 5500,
-    9: 4600,
-    8: 3900,
-    7: 3300,
-    6: 2800,
-    5: 2400,
-    4: 2100,
-    3: 1850,
-    2: 1600,
-    1: 450,
-};
-
-const spellsAndEquipments = [
-    {
-        name: "Lightning",
-        levels: [
-            {level: 1, damage: 150},
-            {level: 2, damage: 180},
-            {level: 3, damage: 210},
-            {level: 4, damage: 240},
-            {level: 5, damage: 270},
-            {level: 6, damage: 320},
-            {level: 7, damage: 400},
-            {level: 8, damage: 480},
-            {level: 9, damage: 560},
-            {level: 10, damage: 600},
-            {level: 11, damage: 640}
-        ]
-    },
-
-    {
-        name: "Earthquake",
-        levels: [
-            {level: 1, damage: 0.145},
-            {level: 2, damage: 0.17},
-            {level: 3, damage: 0.21},
-            {level: 4, damage: 0.25},
-            {level: 5, damage: 0.29}
-        ]
-    },
-
-    {
-        name: "GiantArrow",
-        levels: [
-            {level: 1, damage: 750},
-            {level: 2, damage: 750},
-            {level: 3, damage: 850},
-            {level: 4, damage: 850},
-            {level: 5, damage: 850},
-            {level: 6, damage: 1000},
-            {level: 7, damage: 1000},
-            {level: 8, damage: 1000},
-            {level: 9, damage: 1200},
-            {level: 10, damage: 1200},
-            {level: 11, damage: 1200},
-            {level: 12, damage: 1500},
-            {level: 13, damage: 1500},
-            {level: 14, damage: 1500},
-            {level: 15, damage: 1750},
-            {level: 16, damage: 1750},
-            {level: 17, damage: 1750},
-            {level: 18, damage: 1950}
-        ]
-    },
-
-    {
-        name: "Fireball",
-        levels:  [
-            {level: 1, damage: 1500},
-            {level: 2, damage: 1500},
-            {level: 3, damage: 1700},
-            {level: 4, damage: 1700},
-            {level: 5, damage: 1800},
-            {level: 6, damage: 1950},
-            {level: 7, damage: 1950},
-            {level: 8, damage: 2050},
-            {level: 9, damage: 2200},
-            {level: 10, damage: 2200},
-            {level: 11, damage: 2350},
-            {level: 12, damage: 2650},
-            {level: 13, damage: 2650},
-            {level: 14, damage: 2750},
-            {level: 15, damage: 3100},
-            {level: 16, damage: 3100},
-            {level: 17, damage: 3250},
-            {level: 18, damage: 3400},
-            {level: 19, damage: 3400},
-            {level: 20, damage: 3500},
-            {level: 21, damage: 3650},
-            {level: 22, damage: 3650},
-            {level: 23, damage: 3750},
-            {level: 24, damage: 3900},
-            {level: 25, damage: 3900},
-            {level: 26, damage: 3950},
-            {level: 27, damage: 4100}
-        ],
-    }
-
-];
-
-slider.oninput =  function() {
-    sliderLevel.innerHTML = this.value;
-
-    if(imageMap[this.value]) {
-        cardLogo.src = imageMap[this.value]
-        hp.textContent = hpMap[this.value]
-    }
-};
+const newCardBtn = document.getElementById("newCardBtn");
+const main = document.querySelector(".main");
+const root = document.documentElement;
+const primaryColor = getComputedStyle(root).getPropertyValue("--primary-color")
+const backgroundColor = getComputedStyle(root).getPropertyValue("--background-color");
 
 function getSpellDamage(spellName, level) {
     const spell = spellsAndEquipments.find(spell=>  spell.name === spellName);
@@ -168,6 +42,99 @@ spellSelect.addEventListener("change", updateDamage);
 levelSlider.addEventListener("input", () => {
     levelValue.textContent = levelSlider.value;
     updateDamage();
+});
+
+newCardBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const newCard = document.createElement("div");
+    
+    const exitIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+
+    exitIcon.setAttribute("height", "24px");
+    exitIcon.setAttribute("viewBox", "0 -960 960 960");
+    exitIcon.setAttribute("width", "24px");
+    exitIcon.setAttribute("fill", "#000000");
+
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", "m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z");
+    exitIcon.appendChild(path);
+    newCard.appendChild(exitIcon)
+    exitIcon.style.position = "absolute";
+    exitIcon.style.top = "10px";
+    exitIcon.style.right = "10px"
+    exitIcon.style.cursor = "pointer"
+    
+    exitIcon.addEventListener("click", function() {
+        newCard.remove()
+    })
+
+    newCard.classList.add("card");
+    main.appendChild(newCard)
+    newCard.style.backgroundColor = backgroundColor.trim()
+    newCard.style.width = "250px";
+    newCard.style.height = "250px";
+    newCard.style.position = "relative"
+
+    const buildingLabel = document.createElement("lable");
+    newCard.appendChild(buildingLabel);
+    buildingLabel.textContent = "Select the building: "
+
+    const selection = document.createElement("select");
+    newCard.appendChild(selection);
+
+    const firstOption = document.createElement("option");
+    firstOption.text = "Select the building";
+    selection.appendChild(firstOption)
+
+    Object.keys(buildings).forEach(buildingKey => {
+        const option = document.createElement("option");
+        option.textContent = buildingKey.charAt(0).toUpperCase() + buildingKey.slice(1);
+        option.value = buildingKey;
+        selection.appendChild(option)
+    });
+    
+    selection.addEventListener("change", (event) => {
+        const selectedBuilding = event.target.value;
+        
+        const buildingEntry = Object.entries(buildings).find(([key, value]) => key === selectedBuilding);
+
+        if(buildingEntry) {
+            buildingLabel.remove();
+            selection.remove();
+
+            const buildingInfoContainer = document.createElement("div");
+            newCard.appendChild(buildingInfoContainer);
+
+            const buildingName = document.createElement("h3");
+            buildingInfoContainer.appendChild(buildingName);
+            buildingName.textContent = selectedBuilding.charAt(0).toUpperCase() + selectedBuilding.slice(1)
+
+            const buildingImage = document.createElement("img");
+            buildingImage.src = buildingEntry[1].stats[0].image;
+            buildingImage.style.width = "80px"
+            buildingInfoContainer.appendChild(buildingImage);
+
+            const slider = document.createElement("input");
+            const sliderContainer = document.createElement("div");
+            newCard.appendChild(sliderContainer);
+            sliderContainer.appendChild(slider);
+            slider.type = "range"
+            slider.max = "16"
+            slider.min = "1"
+            slider.step = "1"
+            slider.classList.add("levelSlider")
+
+            slider.addEventListener("change", function() {
+                const level = parseInt(slider.value) - 1;
+
+                
+            })
+            
+        }
+
+    });
 });
 
 updateDamage()
